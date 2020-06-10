433adb4d7f9c22c9e372986f9fb56e1934474253
林　晃弘 <ampmaco@gmail.com>
2020-06-10 12:46:19 +0900

    カテゴリーテーブル生成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_08_164728) do
+ActiveRecord::Schema.define(version: 2020_06_10_034007) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "category_name", unique: true
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
