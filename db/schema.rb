9bbd6a45559faca3fd973501e5230c5b5130f7e6
林　晃弘 <ampmaco@gmail.com>
2020-06-11 10:26:38 +0900

    投稿テーブル、コントローラ、ビュー、ルーティング生成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_034007) do
+ActiveRecord::Schema.define(version: 2020_06_11_011656) do
 
   create_table "categories", force: :cascade do |t|
-    t.string "category_name", unique: true
+    t.string "category_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
