67a27191991233d3d103c9bc67ef3a5f5eea92bb
林　晃弘 <ampmaco@gmail.com>
2020-06-11 10:44:10 +0900

    レスポンス機能モデル、コントローラー、ルーティング生成

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
 	belongs_to :user
 	has_many :categories, through: :post_categories, dependent: :destroy
+	has_many :messages, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :categories, only: [:new, :create, :index]
-  resources :posts, only: [:new, :create, :index, :show]
+  resources :posts, only: [:new, :create, :index, :show] do
+  	resource :messages, only: [:create, :destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
