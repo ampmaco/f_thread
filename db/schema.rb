39c0ee0eea09dc5822a654c26ea370e7b806a6f9
林　晃弘 <ampmaco@gmail.com>
2020-06-11 10:45:21 +0900

    レスポンス機能テーブルのマイグレーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_11_012827) do
+ActiveRecord::Schema.define(version: 2020_06_11_013902) do
 
   create_table "categories", force: :cascade do |t|
     t.string "category_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
