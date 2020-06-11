bb0aa248a0c679c722fecdf35d240b7a28a3db69
林　晃弘 <ampmaco@gmail.com>
2020-06-11 10:37:08 +0900

    カテゴリーと投稿の中間テーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_11_011656) do
+ActiveRecord::Schema.define(version: 2020_06_11_012827) do
 
   create_table "categories", force: :cascade do |t|
     t.string "category_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "post_categories", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "category_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
